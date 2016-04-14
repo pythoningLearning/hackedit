c734a589e1e379656841666cf7cf0a4fb0411e60
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2016-04-14 14:05:46 +0200

    Add a script to create a debian package
    need to be tested on various flavours of ubuntu/debian

---
+++ scripts/make_deb.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# This script is meant to be run on Ubuntu 14.04.
+#
+# Execute the following commands to setup your environment:
+#
+# sudo apt-get install python3-pip python3-all debhelper devscripts
+# sudo pip3 install stdeb
+#
+pushd ..
+
+export DEB_BUILD_OPTIONS=nocheck debuild
+python3 setup.py --command-packages=stdeb.command bdist_deb
