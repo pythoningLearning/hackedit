55b26bb709626905bb0a6bf9b3b240a0f22a1dab
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2016-06-19 19:52:11 +0200

    Change documents view's icon

--- hackedit/plugins/documents.py
+++ hackedit/plugins/documents.py
@@ -4,19 +4,19 @@ This plugin show the list of open documents.
 import os
 from hackedit import api
 
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class OpenDocuments(api.plugins.WorkspacePlugin):
     """ This plugin show the list of open documents. """
     def activate(self):
         self.list = QtWidgets.QListWidget()
-        api.window.add_dock_widget(
-            self.list, _('Documents'),
-            icon=api.widgets.FileIconProvider.mimetype_icon('file.txt'),
-            area=QtCore.Qt.LeftDockWidgetArea)
-        api.signals.connect_slot(
-            api.signals.CURRENT_EDITOR_CHANGED, self._update)
+        if QtGui.QIcon.hasThemeIcon('folder-documents'):
+            icon = QtGui.QIcon.fromTheme('folder-documents')
+        else:
+            icon = api.widgets.FileIconProvider.mimetype_icon('file.txt')
+        api.window.add_dock_widget(self.list, _('Documents'), icon=icon, area=QtCore.Qt.LeftDockWidgetArea)
+        api.signals.connect_slot(api.signals.CURRENT_EDITOR_CHANGED, self._update)
         self.list.itemActivated.connect(self._on_item_activated)
         self.list.itemClicked.connect(self._on_item_activated)
         self.icon_provider = api.widgets.FileIconProvider()
@@ -33,8 +33,7 @@ class OpenDocuments(api.plugins.WorkspacePlugin):
         current_index = 0
         for n, p in zip(names, paths):
             if names.count(n) > 1:
-                n = os.path.join(
-                    QtCore.QFileInfo(os.path.dirname(p)).completeBaseName(), n)
+                n = os.path.join(QtCore.QFileInfo(os.path.dirname(p)).completeBaseName(), n)
             itm = QtWidgets.QListWidgetItem(
                 self.icon_provider.icon(QtCore.QFileInfo(n)), n)
             itm.setToolTip(p)
